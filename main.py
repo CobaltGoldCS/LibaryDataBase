0e8c5ec2ea055041adabd76b9f7fddbd71953906
CobaltGoldCS <54545769+CobaltGoldCS@users.noreply.github.com>
2019-08-27 14:23:35 -0600

    Adding search choices

--- main.py
+++ main.py
@@ -14,10 +14,20 @@ def main():
         addTuple = (addBook, addLN, addGenre, addLocation, addQuantity)
         cursor.execute('INSERT INTO Library VALUES (?,?,?,?,?)', addTuple)
     elif choice == 2:
-        print("How do you wnat to search")
+        print("How do you want to search?\n")
         choice2 = input("Author's last name(1), Book name(2), or Genre(3): ")
         if choice2 == 1:
             useAuth = input("Author's last name: ")
             cursor.execute('SELECT * FROM Library WHERE symbol=?', useAuth)
+        if choice2 == 2:
+            useBook = input("Book name: ")
+            cursor.execute('SELECT * FROM Library WHERE symbol=?', useBook)
+        if choice2 == 3:
+            useGenre = input("Genre: ")
+            cursor.execute('SELECT * FROM Library WHERE symbol=?', useGenre)
+        else:
+            print("Error")
+            main()
     else:
         print("Error")
+        main()
