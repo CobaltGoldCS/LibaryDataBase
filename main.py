bce9cbb9d040987b54b2f9360d490989a85ddafe
CobaltGoldCS <cobaltgoldcsprinks@gmail.com>
2019-09-03 15:12:25 +0000

    master: Improved functionallity

--- main.py
+++ main.py
@@ -1,44 +1,43 @@
 import sqlite3
-import time
+
 conn = sqlite3.connect('lib_database')
 cursor = conn.cursor()
-#Only works if your database is empty, otherwise throws up an error message, which is pretty annoying
-#cursor.execute('''CREATE TABLE Library
-#(BookTitle text, LN text, Genre text, Location text, Quantity tinyint )''')
+
+
 def main():
-    #This is basically all functionallity for everything 
-    choice = input("Do you want to add to library database(1), or find some book(2)?: ")
-    if choice == 1:
+    """Where all magic happens folks"""
+    choice = input("Add (1), find (2)?: ")
+    if choice == "1":
         addBook = input("Bookname: ")
         addLN = input("Author's last name: ")
         addGenre = input("Genre of the book: ")
         addLocation = input("Where is this book found?: ")
         addQuantity = input("How many copies of this book do you have?: ")
-        addTuple = (addBook, addLN, addGenre, addLocation, addQuantity)
-        cursor.execute('INSERT INTO Library VALUES (?,?,?,?,?)', addTuple)
+        addTuple = [(addBook, addLN, addGenre, addLocation, addQuantity)]
+        cursor.executemany('INSERT INTO Library VALUES (?,?,?,?,?)', addTuple)
     # Still needs to use print to show the obtained information
-    elif choice == 2:
+    elif choice == "2":
         print("How do you want to search?\n")
-        choice2 = input("Author's last name(1), Book name(2), Genre(3), or all: ")
-        if choice2 == 1:
+        choice2 = input("Last name(1), Book name(2), Genre(3), or all: ")
+        if choice2 == "1":
             useAuth = input("Author's last name: ")
-            cursor.execute('SELECT * FROM Library WHERE symbol=?', useAuth)
+            cursor.execute('SELECT * FROM Library WHERE symbol=?', (useAuth,))
             rows = cursor.fetchall()
             for row in rows:
                 print(rows)
-        if choice2 == 2:
+        if choice2 == "2":
             useBook = input("Book name: ")
-            cursor.execute('SELECT * FROM Library WHERE symbol=?', useBook)
+            cursor.execute('SELECT * FROM Library WHERE symbol=?', (useBook,))
             rows = cursor.fetchall()
             for row in rows:
                 print(rows)
-        if choice2 == 3:
+        if choice2 == "3":
             useGenre = input("Genre: ")
-            cursor.execute('SELECT * FROM Library WHERE symbol=?', useGenre)
+            cursor.execute('SELECT * FROM Library WHERE symbol=?', (useGenre,))
             rows = cursor.fetchall()
             for row in rows:
                 print(rows)
-        if choice ==4:
+        if choice == "4":
             cursor.execute('SELECT * FROM Library')
             rows = cursor.fetchall()
             for row in rows:
@@ -49,4 +48,6 @@ def main():
     else:
         print("Error")
         main()
+
+
 main()
