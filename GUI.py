6eb80a2b3849e535a7e8d7f1b723b0176b1a5433
CobaltGoldCS <54545769+CobaltGoldCS@users.noreply.github.com>
2019-09-09 10:18:38 -0600

    Add files via upload

--- GUI.py
+++ GUI.py
@@ -1,6 +1,37 @@
 import tkinter
 import sqlite3
 
+
+
+
+
+class functionallity():
+  def createBook():
+    conn = sqlite3.connect('lib_database')
+    cursor = conn.cursor()
+    name = BookEntry.get()
+    location= locationEntry.get()
+    genre=genreEntry.get()
+    LN = LNEntry.get()
+    quantity = quantityEntry.get()
+    addTuple = [(str(name)), (str(LN)), (str(genre)), (str(location)), (str(quantity))]
+    cursor.execute('INSERT INTO Library VALUES (?,?,?,?,?)', addTuple)
+    conn.commit()
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 #Defines the container for the GUI
 container = tkinter.Tk()
 container.title('AAI Library')
@@ -9,8 +40,32 @@ menuBar = tkinter.Menu(container)
 #menuBar.add_cascade(Label="Search", command=)
 
 bookFrame = tkinter.Frame(container)
-bookFrame.pack(side= "top")
-#Need to figure out the class stuff in python so I can understand why this doesnt work for me
-addButton = tkinter.Button(bookFrame, text="Add a book", )
-addButton.pack(side="Left")
+bookFrame.grid(column = 2)
+bookFrame.pack()
+LabelBook = tkinter.Label(bookFrame, text="Book's Name")
+LabelLN = tkinter.Label(bookFrame, text="Author's Last Name")
+labelGenre =  tkinter.Label(bookFrame, text="Book's Genre")
+labelLocation = tkinter.Label(bookFrame, text="Book Location")
+labelQuantity = tkinter.Label(bookFrame, text="How many books does the Library have")
+
+BookEntry = tkinter.Entry(bookFrame)
+LNEntry = tkinter.Entry(bookFrame)
+genreEntry = tkinter.Entry(bookFrame)
+locationEntry = tkinter.Entry(bookFrame)
+quantityEntry = tkinter.Entry(bookFrame)
+
+
+LabelBook.grid(row=1)
+LabelLN.grid(row=2)
+labelGenre.grid(row=3) 
+labelLocation.grid(row=4)
+labelQuantity.grid(row=5)
+
+BookEntry.grid(row=1, column=1)
+LNEntry.grid(row=2, column=1)
+genreEntry.grid(row=3, column=1)
+locationEntry.grid(row=4, column=1)
+quantityEntry.grid(row=5, column=1)
+addButton = tkinter.Button(bookFrame, padx=10, pady=10, bd=2, text= "Add Book", command=functionallity.createBook)
+addButton.grid(row=6, column=1)
 container.mainloop()
